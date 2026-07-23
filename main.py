0bb820a5b2f54e4fe623bf726e2f76fbda06dba7
Mayur Indalkar <mayurindalkar@Mayurs-MacBook-Pro.local>
2026-07-22 22:55:19 -0400

    Add client for math server

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ load_dotenv()
 
 from langchain.agents import create_agent
 from langchain_mcp_adapters.tools import load_mcp_tools
+from langchain_core.messages import HumanMessage
 from langchain_openai import ChatOpenAI
 from mcp import ClientSession, StdioServerParameters
 from mcp.client.stdio import stdio_client
@@ -18,7 +19,19 @@ stdio_server_params = StdioServerParameters(
 )
 
 async def main():
-    print("Hello from langchain-learning!")
+    async with stdio_client(stdio_server_params) as (read, write):
+        async with ClientSession(read_stream=read, write_stream=write) as session:
+            await session.initialize()
+            print("session initialized")
+            tools = await load_mcp_tools(session=session)
+        
+            agent = create_agent(llm, tools=tools)
+            result = await agent.ainvoke(
+                {"messages": [HumanMessage(content="What is 54 + 2 * 3?")]}
+            )
+            print(result["messages"][-1].content)
+
+
 
 
 if __name__ == "__main__":
